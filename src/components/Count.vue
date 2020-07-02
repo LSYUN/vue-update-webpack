2b92b6480e154f43069675408b6e7b453cf15f5d
lushiyun1 <lushiyun1@timesneighborhood.cn>
2020-07-02 10:18:47 +0800

    first commit

---
+++ src/components/Count.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>this is Count</div>
+    <div>{{obj.b}}</div>
+  </div>
+</template>
+<script>
+import _ from 'lodash'
+export default {
+  name: 'Count',
+  data() {
+    return {
+      obj: {
+        a: 'a',
+        b: 'b'
+      }
+    }
+  },
+  mounted() {
+    this.obj = _.omit(this.obj, 'a')
+    console.log('this.obj', this.obj)
+  }
+}
+
+</script>
